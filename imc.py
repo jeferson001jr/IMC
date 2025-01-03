4ecdac5b567c3a6bb75a20cc8766f5398d1fe06f
jeferson001jr <juchiquesi@gmail.com>
2025-01-02 22:56:59 -0300

    calculando imc

--- imc.py
+++ imc.py
@@ -1,20 +1,16 @@
-altura = float(input("Digite sua altura: "))
-peso = float(input("Digite seu peso: "))
-imc = altura * altura / peso
+altura = float(input("Digite sua altura (em metros): "))
+peso = float(input("Digite seu peso (em quilogramas): "))
+imc = peso / (altura * altura)
 
-if imc <= 18.5 :
+if imc <= 18.5:
     print("Seu IMC é igual a baixo peso")
-if imc >= 18.5:
+elif 18.5 < imc <= 24.9:
     print("Seu IMC é igual a peso adequado")
-if imc >= 24.9:
+elif 24.9 < imc <= 29.9:
     print("Seu IMC é igual a sobrepeso")
-if imc >= 29.9:
+elif 29.9 < imc <= 34.9:
     print("Seu IMC é igual a obesidade grau I")
-if imc >= 34.9:
+elif 34.9 < imc <= 39.9:
     print("Seu IMC é igual a obesidade grau II")
-if imc >= 39.9:  
-    print("Seu IMC é igual a obesidade grau III")
 else:
-    print("Encerrando programa")
-    
-
+    print("Seu IMC é igual a obesidade grau III")
